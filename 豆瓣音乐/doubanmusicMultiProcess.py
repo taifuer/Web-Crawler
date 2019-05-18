bdf7c0fa54a01cd4777220f9ec0b294ffc0aa4f9
taifu <taifu@taifua.com>
2019-05-18 23:31:17 +0800

    豆瓣音乐多进程爬虫

--- 豆瓣音乐/doubanmusicMultiProcess.py
+++ 豆瓣音乐/doubanmusicMultiProcess.py
@@ -1,5 +1,12 @@
 # -*- coding:utf-8 -*-
 """
+@name:   doubanmusicMultiProcess.py
+@time:   2019 / 05 / 18
+@author: taifu
+"""
+
+# -*- coding:utf-8 -*-
+"""
 @name:   doubanmusicMultiThread.py
 @time:   2019 / 05 / 18
 @author: taifu
@@ -8,7 +15,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-import threading
+from multiprocessing import Pool
 import datetime
 
 # 请求头
@@ -17,7 +24,7 @@ headers = {
 }
 
 # 获取页面html代码
-def getOnePage(url, threadName):
+def getOnePage(url):
     try:
         r = requests.get(url, headers=headers)
         if r.status_code == 200:
@@ -31,7 +38,7 @@ def getOnePage(url, threadName):
 id = 0
 
 # 获取指定内容
-def parseOnePage(html, threadName):
+def parseOnePage(html):
     soup = BeautifulSoup(html, 'lxml')
     # 专辑id
     global id
@@ -73,42 +80,16 @@ def parseOnePage(html, threadName):
     return info
 
 # 获取音乐页面URL
-def getMusicUrl(url, threadName):
-    indexHtml = getOnePage(url, threadName)
+def getMusicUrl(url):
+    indexHtml = getOnePage(url)
     soup = BeautifulSoup(indexHtml, 'lxml')
     aTags = soup.find_all('a', class_='nbg')
     for aTag in aTags:
         itemUrl = aTag.attrs['href']
-        itemHtml = getOnePage(itemUrl, threadName)
-        itemInfo = parseOnePage(itemHtml, threadName)
-        print(threadName, itemInfo)
+        itemHtml = getOnePage(itemUrl)
+        itemInfo = parseOnePage(itemHtml)
+        print(itemInfo)
 
-# 获取URL
-lock = threading.Lock()
-def getUrl():
-    global urls
-    lock.acquire()
-    if len(urls) == 0:
-        lock.release()
-        return ''
-    else:
-        url = urls[0]
-        del urls[0]
-    lock.release()
-    return url
-
-# 多线程爬虫
-class SpiderThread(threading.Thread):
-    def __init__(self, name):
-        threading.Thread.__init__(self)
-        self.name = name
-    def run(self):
-        while True:
-            url = getUrl()
-            if url != '':
-                getMusicUrl(url, self.name)
-            else:
-                break
 
 
 # 入口
@@ -118,20 +99,8 @@ if __name__ == '__main__':
     # 获取多个链接
     urls = ['https://music.douban.com/top250?start={}'.format(i * 25) for i in range(0, 10)]
 
-    thread1 = SpiderThread('thread1')
-    thread2 = SpiderThread('thread2')
-    thread3 = SpiderThread('thread3')
-    thread4 = SpiderThread('thread4')
-
-    thread1.start()
-    thread2.start()
-    thread3.start()
-    thread4.start()
-
-    thread1.join()
-    thread2.join()
-    thread3.join()
-    thread4.join()
+    pool = Pool(processes=4)
+    pool.map(getMusicUrl, urls)
 
     endTime = datetime.datetime.now()
     print('耗费时间', (endTime - startTime).seconds, '秒')
